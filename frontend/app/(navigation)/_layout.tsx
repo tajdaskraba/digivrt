04c764ce8bcdd228f1eca07978f699835e216cf4
tajdaskraba <tajda.skraba@gmail.com>
2025-04-09 19:19:53 +0200

    revert index file rename

--- frontend/app/(navigation)/_layout.tsx
+++ frontend/app/(navigation)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="components"
+        name="index"
         options={{
           title: 'Komponente',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
